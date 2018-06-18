e59edd906861567bdee56cb4cee0efd341002626
hamzab97 <hbana3@uwo.ca>
2018-06-18 07:00:33 -0400

    initial commit

--- src/pages/messenger/messenger.ts
+++ src/pages/messenger/messenger.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
-import { AngularFirestore } from 'angularfire2/firestore';
+import {IonicPage, Item, NavController, NavParams} from 'ionic-angular';
 import {AngularFireDatabase} from "angularfire2/database";
-import {FirebaseListObservable} from "angularfire2/database-deprecated";
+import CryptoJS from 'crypto-js';
+import { map } from 'rxjs/operators';
+import {AngularFirestoreCollection} from "angularfire2/firestore";
+import {Observable} from "rxjs/Observable";
+import {Subscription} from "rxjs/Subscription";
 
 /**
  * Generated class for the MessengerPage page.
@@ -16,21 +19,42 @@ import {FirebaseListObservable} from "angularfire2/database-deprecated";
   selector: 'page-messenger',
   templateUrl: 'messenger.html',
 })
+
 export class MessengerPage {
   username: string='';
   message: string='';
+  encryptedMessages: object[] = [];
+  private subscription: Subscription;
 
   constructor(public db: AngularFireDatabase, public navCtrl: NavController, public navParams: NavParams) {
     this.username = this.navParams.get('username');
     this.message = this.navParams.get('message');
-    console.log(this.navParams);
+    this.subscription = this.db.list('messenger').valueChanges().map(elem =>{
+      this.encryptedMessages.push(elem);
+      console.log(elem);
+    }).subscribe();
+    // for (let m in this.encryptedMessages){
+    //   m = m.toString(CryptoJS.enc.Utf8);
+    // }
+    console.log('hoiluuluu')
+  }
+
+  getDecryptedMessages(){
+    return this.encryptedMessages;
   }
 
   sendMessage(){
-    db.list("/messenger").push({
+    this.message = CryptoJS.AES.encrypt(this.message, 'secret key 123');
+    let bytes = CryptoJS.AES.decrypt(this.message.toString(), 'secret key 123');
+    this.db.list("/messenger").push({
       username: this.username,
-      message: this.message
-    }); //access database and push new message
+      message: bytes
+    }) //access database and push new message
+    .then(()=>{
+      //what happens once message is sent
+    });
+    // let bytes = CryptoJS.AES.decrypt(this.message.toString(), 'secret key 123');
+    // let plaintext = bytes.toString(CryptoJS.enc.Utf8);
   }
 
   ionViewDidLoad() {
